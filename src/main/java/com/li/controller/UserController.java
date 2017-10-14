02314040850fa34b4e003bd5f7e338f0d6d63db9
liqin <1179139256@qq.com>
2017-10-14 09:28:52 +0800

    20171014lqin

--- src/main/java/com/li/controller/UserController.java
+++ src/main/java/com/li/controller/UserController.java
@@ -4,11 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.li.model.UserEntity;
 import com.li.service.UserService;
@@ -27,4 +30,12 @@ public class UserController {
         map.put("loginResult", loginResult);
         return map;
     }
+    
+   
+	@RequestMapping("/loginIn")
+	public ModelAndView loginIn(HttpServletRequest request,HttpServletResponse response){
+		ModelAndView modelAndView=new ModelAndView("/index.jsp");
+		return modelAndView;
+	}
+    
 }
